04899cd1ebc2a9c63b254d2389dd444d391d2554
Narek <narek1144@gmail.com>
2022-09-01 22:23:21 +0400

    feat: add dark mode

--- src/index.ts
+++ src/index.ts
@@ -4,35 +4,37 @@ import { initSettingBtns } from "./modules/cv-render/settingshandlers";
 import { drawCV } from "./modules/cv-render/drawcv";
 import { currentCVData } from "./modules/cv-render/cvdata";
 import { doc } from "prettier";
+import { choosTopic } from "./modules/theme/theme";
 
-function init(){
-  switch(document.body.dataset.page){
-    case 'cv-main':
+function init() {
+  switch (document.body.dataset.page) {
+    case "cv-main":
       initCVMainPage();
       break;
-    case 'cv-create':
+    case "cv-create":
       initCVCreatePage();
       break;
-    case 'index':
+    case "index":
       initIndexPage();
       break;
   }
 }
 
-function initCVCreatePage(){
-  const container = document.querySelector('.canvas-container') as HTMLElement;
+function initCVCreatePage() {
+  const container = document.querySelector(".canvas-container") as HTMLElement;
   drawCV(currentCVData, container);
   initSettingBtns();
 }
 
-function initCVMainPage(){
-  const cvThumbs = document.querySelectorAll('.cv-thumb');
+function initCVMainPage() {
+  const cvThumbs = document.querySelectorAll(".cv-thumb");
   cvThumbs.forEach((el) => drawCV(currentCVData, el as HTMLElement));
 }
 
-function initIndexPage(){
-  const cvThumbs = document.querySelectorAll('.cv-thumb');
+function initIndexPage() {
+  const cvThumbs = document.querySelectorAll(".cv-thumb");
   cvThumbs.forEach((el) => drawCV(currentCVData, el as HTMLElement));
 }
 
 init();
+choosTopic();
